5f396572a2499a8d1b5086978ea9860e2d658a6b
Pauljosueee <pjmuela@sudamericano.edu.ec>
2024-12-17 13:48:24 -0500

    code

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+  
+ app.enableCors({
+  origin: 'http://localhost:4200',
+ });
 
   const documentOptions = new DocumentBuilder()
  .setTitle('REST API')
